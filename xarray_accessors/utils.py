17f308f1ee2940c27940efddadff9bcd58b3d5d0
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-10-10 17:34:19 +0900

    #1 Add has_nested_attr function

--- xarray_accessors/utils.py
+++ xarray_accessors/utils.py
@@ -2,6 +2,7 @@ __all__ = [
     "get_nested_attr",
     "set_nested_attr",
     "del_nested_attr",
+    "has_nested_attr",
 ]
 
 
@@ -111,3 +112,26 @@ def del_nested_attr(obj: Any, names: Sequence[str]) -> None:
         delattr(obj, names[0])
     else:
         delattr(get_nested_attr(obj, names[:-1]), names[-1])
+
+
+def has_nested_attr(obj: Any, names: Sequence[str]) -> bool:
+    """Return whether an object has a nested attribute.
+
+    Args:
+        obj: Object to be evaluated.
+        names: Sequence of attribute names.
+
+    Returns:
+        `True` if the object has the nested attribute. `False` otherwise.
+
+    Raises:
+        AttributeError: Raised if the nested attribute does not exist.
+        ValueError: Raised if `names` is an invalid object
+            (e.g., a string, an empty list or tuple).
+
+    """
+    try:
+        get_nested_attr(obj, names)
+        return True
+    except AttributeError:
+        return False
