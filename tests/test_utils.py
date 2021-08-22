eca63bbaf7d5102e6a71cd31e7bb96deb5fd8d58
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-08-22 22:31:19 +0900

    #3 Reorder functions in tests for utils module

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,10 +4,10 @@ from pytest import raises
 
 # submodules
 from xarray_accessors.utils import (
-    get_nested_attr,
-    set_nested_attr,
     del_nested_attr,
+    get_nested_attr,
     has_nested_attr,
+    set_nested_attr,
 )
 
 
@@ -23,19 +23,6 @@ data.a.b.c = "data"  # type: ignore
 
 
 # test functions
-def test_get_nested_attr() -> None:
-    names = ("a", "b", "c")
-    expected = data.a.b.c  # type: ignore
-    assert get_nested_attr(data, names) == expected
-
-
-def test_set_nested_attr() -> None:
-    names = ("a", "b", "d")
-    expected = "data"
-    set_nested_attr(data, names, expected)
-    assert data.a.b.d == expected  # type: ignore
-
-
 def test_del_nested_attr() -> None:
     names = ("a", "b", "e")
     data.a.b.e = "data"  # type: ignore
@@ -45,6 +32,19 @@ def test_del_nested_attr() -> None:
         data.a.b.e  # type: ignore
 
 
+def test_get_nested_attr() -> None:
+    names = ("a", "b", "c")
+    expected = data.a.b.c  # type: ignore
+    assert get_nested_attr(data, names) == expected
+
+
 def test_has_nested_attr() -> None:
     names = ("a", "b", "c")
     assert has_nested_attr(data, names)
+
+
+def test_set_nested_attr() -> None:
+    names = ("a", "b", "d")
+    expected = "data"
+    set_nested_attr(data, names, expected)
+    assert data.a.b.d == expected  # type: ignore
