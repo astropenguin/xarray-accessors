c22b89a2c5e000b5ea1fdcca34af5aa4359053a8
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-08-25 17:48:08 +0900

    #5 Add accessor module

--- xarray_accessors/__init__.py
+++ xarray_accessors/__init__.py
@@ -1,7 +1,9 @@
-# metadata
+# flake8: noqa
+# type: ignore
 __author__ = "Akio Taniguchi"
 __version__ = "0.1.0"
 
 
 # submodules
-from . import utils  # noqa
+from . import accessor
+from . import utils
